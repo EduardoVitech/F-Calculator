60ddd6212774f68503b337d1013c03b03115bdb1
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-05-26 09:08:20 -0300

    Implementando Lógica

--- lib/pages/calculator_page.dart
+++ lib/pages/calculator_page.dart
@@ -2,6 +2,7 @@ import 'package:fcalculator/components/display/display.dart';
 import 'package:fcalculator/components/keyboard/keyboard.dart';
 import 'package:fcalculator/models/memory/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculatorPage extends StatefulWidget {
   @override
@@ -19,6 +20,10 @@ class _CalculatorPageState extends State<CalculatorPage> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Column(
